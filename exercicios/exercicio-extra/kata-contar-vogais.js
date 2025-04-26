d7643d775a27e5f70531a374339c9e7f0cb21b09
Matheus Leao <matsouzaleao@gmail.com>
2025-04-26 18:33:51 -0300

    katar de contar vogais

--- exercicios/exercicio-extra/kata-contar-vogais.js
+++ exercicios/exercicio-extra/kata-contar-vogais.js
@@ -3,3 +3,22 @@ Retorne o número de vogais no texto fornecido. Consideraremos a, e, i, o e u co
 este Kata. O texto de entrada conterá apenas letras minúsculas e/ou espaços. Letras acentuadas 
 não fazem parte desse desafio.
 */
+
+const texto1 = 'matheus souza leao teste'
+const texto2 = 'matheus'
+const listVogais = ['a', 'e', 'i', 'o', 'u']
+
+const getQuantasVogaisExistemNaLista = (texto) => {
+    let textoTratado = texto.toLowerCase().trim().split('');
+    let quantidadeVogaisTotais = 0
+    textoTratado.forEach((letter, index) => {
+        if(listVogais.includes(letter)){
+            quantidadeVogaisTotais++
+        }
+    });
+    return quantidadeVogaisTotais
+
+}
+
+console.log(`O texto 1 contem ${getQuantasVogaisExistemNaLista(texto1)} vogais.`)
+console.log(`O texto 2 contem ${getQuantasVogaisExistemNaLista(texto2)} vogais.`)
