a91034c9fd745184680fd780f5e77262159fe003
Matheus Leao <matsouzaleao@gmail.com>
2025-05-10 16:23:05 -0300

    Criar exercicio dos petiscos

---
+++ exercicios/aula3/petisco-modularizacao/petisco-controller.js
@@ -0,0 +1,17 @@
+/**
+
+  Distribuidor de petiscos
+
+  Você tem uma lista de cães: Pantera, Luna e Thor
+  Crie um script que "entregue" 1 petisco para cada cão da lista.
+  Exiba uma mensagem como: "Entregando petisco para Pantera"
+
+  Mantenha os dados e a função em arquivos separados; use modularização para organizar.
+
+ */
+
+import { CATS, DOGS } from "./petisco-data.js";
+import { entregarPetiscos } from "./petisco-view.js";
+
+entregarPetiscos(DOGS);
+entregarPetiscos(CATS);
